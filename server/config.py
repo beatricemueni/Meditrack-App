b10185635552cb49245d3d62bd367d892dc9c0ae
beatricemueni <muenisophie8@gmail.com>
2026-08-03 10:02:25 +0300

    chore: update config.py to use postgresql database connection string

--- server/config.py
+++ server/config.py
@@ -2,9 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+    
+    
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "sqlite:///meditrack.db"
+        "postgresql://postgres:admin123@localhost:5432/meditrack_db"
     )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret-key")
